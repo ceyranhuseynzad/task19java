6cc2cb16119418220c7679787e272253f415f57b
ceyranhuseynzad <ceyranhuseynzad@gmail.com>
2023-11-02 17:01:12 +0400

    ss

---
+++ script.js
@@ -0,0 +1,50 @@
+//Data1
+//1
+let nihadceki=78;
+let nailceki=92;
+let nihadboy=169;
+let nailboy=195;
+console.log("Nihadin cekisi="+nihadceki+"dir");
+console.log("Nailin cekisi="+nailceki + "dir");
+console.log("Nihadin boyu="+nihadboy+"dir");
+console.log("Nailin boyu="+nailboy+"dir");
+//2
+let BKI=nihadceki/(nihadboy**2);
+console.log("Beden kutle indeksi="+BKI+"dir")
+let BKI1=nailceki/(nailboy**2);
+console.log("Beden kutle indeksi="+BKI1+"dir")
+//3
+if(BKI>BKI1){
+    console.log(Boolean(BKI));
+
+}
+else{
+    console.log(Boolean(NaN));
+
+
+}
+//Data2
+//1
+let nihad_ceki=95;
+let nail_ceki=85;
+let nihad_boy=188;
+let nail_boy=176;
+console.log("Nihadin cekisi="+nihad_ceki+"dir");
+console.log("Nailin cekisi="+nail_ceki+"dir");
+console.log("Nihadin boyu="+nihad_boy+"dir");
+console.log("Nailin boyu="+nail_boy+"dir");
+//2
+let BKI2=nihad_ceki/(nihad_boy**2);
+console.log("Beden kutle indeksi="+BKI2+"dir")
+let BKI3=nail_ceki/(nail_boy**2);
+console.log("Beden kutle indeksi="+BKI3+"dir")
+//3
+if(BKI2>BKI3){
+    console.log(Boolean(BKI3));
+
+}
+else{
+    console.log(Boolean(NaN));
+
+
+}
